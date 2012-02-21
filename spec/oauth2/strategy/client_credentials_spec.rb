75e723d616eeaf3095970ad1618dca9b3a6f7404
Adnan Karač <admin@Admins-MacBook-Pro-2.local>
2012-02-21 13:43:57 +0100

    added client credentials to client

---
+++ spec/oauth2/strategy/client_credentials_spec.rb
@@ -0,0 +1,14 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe ClientCredentials do
+  before(:each) do
+    @client_credentials = ClientCredentials.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
